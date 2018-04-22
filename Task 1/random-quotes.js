e51c10ab6b9d86b806f9f1663b0187edf79f8bd9
SamAkinosun <33718937+SamAkinosun@users.noreply.github.com>
2018-04-22 11:05:56 +0100

    bit test and while loop implemented
    new commit

--- Task 1/random-quotes.js
+++ Task 1/random-quotes.js
@@ -6,9 +6,12 @@ displayRandomQuote = function () {
         3: "The best thing about a boolean is even if you are wrong, you are only off by a bit",
         4: "Ubuntu is an ancient African word, meaning 'can’t configure Debian'",
         5: "Without requirements or design, programming is the art of adding bugs to an empty 'text' file."
-
     }
-    var rand = Math.floor(Math.random() * Object.keys(quotes).length);
+    var rand =0;
+    rand = Math.floor(Math.random() * Object.keys(quotes).length);
+    while (!(rand & 1)) {
+        rand = Math.floor(Math.random() * Object.keys(quotes).length);   
+    }
     console.log(quotes[rand]);
 }
 displayRandomQuote();
